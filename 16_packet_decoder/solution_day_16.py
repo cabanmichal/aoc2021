763cdf4cb8f6ed92d3fe6bd67ac0ea15ed03d943
Michal Caban <cabanmichal@gmail.com>
2021-12-17 09:42:19 +0100

    day 16 - reformatting + refactoring

--- 16_packet_decoder/solution_day_16.py
+++ 16_packet_decoder/solution_day_16.py
@@ -1,27 +1,28 @@
 """https://adventofcode.com/2021/day/16"""
 
+import math
 from abc import ABC, abstractmethod
 from typing import Tuple, List
-import math
 
 
 class Packet(ABC):
-    def __init__(self, bits: str) -> None:
+    def __init__(self, version: int, type_id: int, bits: str) -> None:
+        self.version = version
+        self.type_id = type_id
         self.bits = bits
-        self.version, self.type_id = read_header(bits)
         self.children: List["Packet"] = []
         self.consumed_by_children = 0
-        self.value = 0
 
     @abstractmethod
     def trailing_data(self) -> str:
-        ...
+        """Return bits that should be used for parsing other Packets."""
 
     @abstractmethod
     def make_children(self) -> str:
-        ...
+        """Create child Packets and return bits they didn't use."""
 
     def version_sum(self) -> int:
+        """Solution to part 1."""
         total = self.version
         for child in self.children:
             total += child.version_sum()
@@ -29,8 +30,9 @@ class Packet(ABC):
         return total
 
     def get_value(self) -> int:
+        """Solution to part 2."""
         if not self.children:
-            return self.value
+            raise ValueError("Cannot compute value without children.")
 
         if self.type_id == 0:
             return sum(c.get_value() for c in self.children)
@@ -52,10 +54,10 @@ class Packet(ABC):
         raise ValueError(f"Unknown type id: {self.type_id}")
 
 
-class PacketLiteral(Packet):
-    def __init__(self, bits: str) -> None:
-        super().__init__(bits)
-        self.value, self._data_size = self._parse_data()
+class LiteralPacket(Packet):
+    def __init__(self, version: int, type_id: int, bits: str) -> None:
+        super().__init__(version, type_id, bits)
+        self._value, self._data_size = self._parse_data()
 
     def _parse_data(self) -> Tuple[int, int]:
         bit_groups = []
@@ -67,9 +69,9 @@ class PacketLiteral(Packet):
 
         bit_string = "".join(bit_groups)
         value = int(bit_string, 2)
-        i = 6 + len(bit_string) + len(bit_groups)
+        data_size = 6 + len(bit_string) + len(bit_groups)
 
-        return value, i
+        return value, data_size
 
     def trailing_data(self) -> str:
         return self.bits[self._data_size :]
@@ -77,12 +79,19 @@ class PacketLiteral(Packet):
     def make_children(self) -> str:
         return self.trailing_data()
 
+    def get_value(self) -> int:
+        return self._value
 
-class PacketOperator(Packet):
-    @property
-    @abstractmethod
-    def type_bits_count(self) -> int:
-        ...
+
+class OperatorPacket(Packet):
+    def __init__(
+        self, version: int, type_id: int, bits: str, type_bits_count: int
+    ) -> None:
+        super().__init__(version, type_id, bits)
+        self.type_bits_count = type_bits_count
+        self.children_stop_value = int(
+            self.bits[6 + 1 : 6 + 1 + self.type_bits_count], 2
+        )
 
     @abstractmethod
     def should_make_more_children(self) -> bool:
@@ -96,43 +105,37 @@ class PacketOperator(Packet):
         data = self.trailing_data()
         while self.should_make_more_children():
             child = packet_factory(data)
+            self.children.append(child)
             data_after_child = child.make_children()
             self.consumed_by_children += len(data) - len(data_after_child)
             data = data_after_child
-            self.children.append(child)
         return self.trailing_data()[self.consumed_by_children :]
 
 
-class PacketOp0(PacketOperator):
-    @property
-    def type_bits_count(self) -> int:
-        return 15
+class OperatorPacket0(OperatorPacket):
+    def __init__(self, version: int, type_id: int, bits: str) -> None:
+        super().__init__(version, type_id, bits, 15)
 
     def should_make_more_children(self) -> bool:
-        for_children = int(self.bits[6 + 1 : 6 + 1 + self.type_bits_count], 2)
-        return self.consumed_by_children < for_children
+        return self.consumed_by_children < self.children_stop_value
 
 
-class PacketOp1(PacketOperator):
-    @property
-    def type_bits_count(self) -> int:
-        return 11
+class OperatorPacket1(OperatorPacket):
+    def __init__(self, version: int, type_id: int, bits: str) -> None:
+        super().__init__(version, type_id, bits, 11)
 
     def should_make_more_children(self) -> bool:
-        needed_children = int(self.bits[6 + 1 : 6 + 1 + self.type_bits_count], 2)
-        return len(self.children) < needed_children
+        return len(self.children) < self.children_stop_value
 
 
 def packet_factory(bits: str) -> Packet:
-    _, type_id = read_header(bits)
+    version, type_id = read_header(bits)
     if type_id == 4:
-        return PacketLiteral(bits)
-
+        return LiteralPacket(version, type_id, bits)
     if bits[6] == "0":
-        return PacketOp0(bits)
-
+        return OperatorPacket0(version, type_id, bits)
     if bits[6] == "1":
-        return PacketOp1(bits)
+        return OperatorPacket1(version, type_id, bits)
 
     raise ValueError("Cannot determine packet type from provided bits.")
 
@@ -145,6 +148,7 @@ def read_header(bits: str) -> Tuple[int, int]:
 
 
 def padded_length(number: int, chunk_size: int = 4) -> int:
+    """Return closest gte multiple of chunk_size."""
     full, rem = divmod(number, chunk_size)
     return (full + 1) * chunk_size if rem else full * chunk_size
 
@@ -161,5 +165,5 @@ if __name__ == "__main__":
     initial_bits = parse_input("input.txt")
     packet = packet_factory(initial_bits)
     packet.make_children()
-    print(packet.version_sum())  # 951
-    print(packet.get_value())  # 902198718880
+    for idx, solution in enumerate([packet.version_sum(), packet.get_value()]):
+        print(f"Solution {idx + 1}: {solution:12}")  # 951, 902198718880
